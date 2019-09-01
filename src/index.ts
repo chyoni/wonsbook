77c559868495672f4ad9f76901d9e46ae29656f9
Chiwon <chiwon99881@gmail.com>
2019-09-01 20:13:29 +0900

    GraphQL Server Setting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Test!!!");
+import "./env";
+import app from "./app";
+import { Options } from "graphql-yoga";
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND: string = "/playground";
+const ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
+  port: PORT,
+  playground: PLAYGROUND,
+  endpoint: ENDPOINT
+};
+
+const callBack = () =>
+  console.log(`server on http://localhost:${PORT}/playground ✔`);
+
+app.start(appOptions, callBack);
